1ec6e2b42c7599ce4dd144d6353bd8b7404655af
Z-L-D <zacldoyle@gmail.com>
2022-06-13 17:49:09 -0500

    -

--- program.js
+++ program.js
@@ -2,7 +2,7 @@ const dgram = require('dgram');
 
 const socket_object = dgram.createSocket('udp4');
 
-var command = "Hello From Javascript";
+var command = "Hello from Javascript";
 var address = "127.0.0.1";
 var port = 5500;
 
